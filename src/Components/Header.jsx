efa22bed8eda5720da8744ae60695a6bff6df644
SuhaibAG <suhaib.ag.49@gmail.com>
2024-12-02 14:56:09 +0300

    Added the ipynb and updated csv

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+const Header = () =>{
+    return(
+            <nav>
+                <ul>
+                    <h2>FCIT Course Picker</h2>
+                </ul>
+            </nav>
+    )
+}
+
+export default Header;
---
+++ src/Components/Schedule.jsx
@@ -0,0 +1,30 @@
+import './Schedule.css'
+const Schedule = () =>{
+    return(
+        <div className='schedule-div'>
+            <table>
+                <tr className="table-header">
+                    <th>
+                        thing
+                    </th>
+                    <th>
+                        thing
+                    </th>
+                    <th>
+                        thing
+                    </th>
+                    <th>
+                        thing
+                    </th>
+                    <th>
+                        thing
+                    </th>
+                    
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+
+export default Schedule;
